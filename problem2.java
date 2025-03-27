e4d3243732918122b61ea626071a2ae9258146ed
Ankit-1008-singh <ankit.singh.tech13@gmail.com>
2025-03-27 10:29:24 +0530

    Add files via upload

---
+++ problem2.java
@@ -0,0 +1,24 @@
+public class problem2 {
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int remains=0;
+        ListNode temp=new ListNode(0);
+        ListNode current=temp;
+        while(l1!=null || l2!=null || remains!=0){
+            int val1=(l1!=null)?l1.val:0;
+            int val2=(l2!=null)?l2.val:0;
+            int sum=val1+val2+remains;
+            remains=sum/10;
+            current.next=new ListNode(sum%10);
+            current=current.next;
+            if(l1!=null){
+                l1=l1.next;
+            }
+            if(l2!=null){
+                l2=l2.next;
+            }
+            
+        }
+        return temp.next;
+    }
+}
